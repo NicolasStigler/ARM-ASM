7deb835cd3555bd0faa55f2a2efa9b4a833491ec
tamitooo <162748368+tamitooo@users.noreply.github.com>
2024-11-14 17:39:45 -0500

    Update datapath.v

--- project_5/project_5.srcs/sources_1/new/datapath.v
+++ project_5/project_5.srcs/sources_1/new/datapath.v
@@ -9,6 +9,7 @@ module datapath (
 	ALUSrc,
 	ALUControl,
 	MemtoReg,
+	MemWrite,
 	PCSrc,
 	ALUFlags,
 	PC,
@@ -25,13 +26,15 @@ module datapath (
 	input wire ALUSrc;
 	input wire [1:0] ALUControl;
 	input wire MemtoReg;
+	input wire MemWrite;
 	input wire PCSrc;
 	output wire [3:0] ALUFlags;
 	output wire [31:0] PC;
 	input wire [31:0] Instr;
 	output wire [31:0] ALUResult;
 	output wire [31:0] WriteData;
-	input wire [31:0] ReadData;
+	output wire [31:0] ReadData;
+
 	wire [31:0] PCNext;
 	wire [31:0] PCPlus4;
 	wire [31:0] PCPlus8;
@@ -41,28 +44,33 @@ module datapath (
 	wire [31:0] Result;
 	wire [3:0] RA1;
 	wire [3:0] RA2;
+
 	mux2 #(32) pcmux(
 		.d0(PCPlus4),
 		.d1(Result),
 		.s(PCSrc),
 		.y(PCNext)
 	);
+
 	flopr #(32) pcreg(
 		.clk(clk),
 		.reset(reset),
 		.d(PCNext),
 		.q(PC)
 	);
+
 	adder #(32) pcadd1(
 		.a(PC),
 		.b(32'b100),
 		.y(PCPlus4)
 	);
+
 	adder #(32) pcadd2(
 		.a(PCPlus4),
 		.b(32'b100),
 		.y(PCPlus8)
 	);
+
 	mux2 #(4) ra1mux(
 		.d0(Instr[19:16]),
 		.d1(4'b1111),
@@ -75,6 +83,7 @@ module datapath (
 		.s(RegSrc[1]),
 		.y(RA2)
 	);
+
 	regfile rf(
 		.clk(clk),
 		.we3(RegWrite),
@@ -86,28 +95,40 @@ module datapath (
 		.rd1(SrcA),
 		.rd2(WriteData)
 	);
+
 	mux2 #(32) resmux(
 		.d0(ALUResult),
 		.d1(ReadData),
 		.s(MemtoReg),
 		.y(Result)
 	);
+
 	extend ext(
 		.Instr(Instr[23:0]),
 		.ImmSrc(ImmSrc),
 		.ExtImm(ExtImm)
 	);
+
 	mux2 #(32) srcbmux(
 		.d0(WriteData),
 		.d1(ExtImm),
 		.s(ALUSrc),
 		.y(SrcB)
 	);
+
 	alu alu(
-		SrcA,
-		SrcB,
-		ALUControl,
-		ALUResult,
-		ALUFlags
+		.SrcA(SrcA),
+		.SrcB(SrcB),
+		.ALUControl(ALUControl),
+		.ALUResult(ALUResult),
+		.ALUFlags(ALUFlags)
+	);
+	memory_unit memory (
+		.clk(clk),
+		.we(MemWrite),
+		.addr(ALUResult),
+		.wdata(WriteData),
+		.rdata(ReadData)
 	);
 endmodule
+
