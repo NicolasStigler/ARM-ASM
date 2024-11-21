d113e1d019db7b7916c709b89d5c74893f7ff7f4
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-21 17:17:56 -0500

    Update arm.v

--- project_5/project_5.srcs/sources_1/new/arm.v
+++ project_5/project_5.srcs/sources_1/new/arm.v
@@ -1,23 +1,15 @@
 `timescale 1ns / 1ps
 
 module arm (
-	clk,
-	reset,
-	PC,
-	Instr,
-	MemWrite,
-	ALUResult,
-	WriteData,
-	ReadData
-);
 	input wire clk;
 	input wire reset;
 	output wire [31:0] PC;
-	input wire [31:0] Instr;
+	input wire [31:0] InstrF;
 	output wire MemWrite;
-	output wire [31:0] ALUResult;
-	output wire [31:0] WriteData;
+	output wire [31:0] ALUResultM;
+	output wire [31:0] WriteDataM;
 	input wire [31:0] ReadData;
+);
 	wire [3:0] ALUFlags;
 	wire RegWrite;
 	wire ALUSrc;
@@ -26,10 +18,24 @@ module arm (
 	wire [1:0] RegSrc;
 	wire [1:0] ImmSrc;
 	wire [1:0] ALUControl;
+
+	wire StallF, StallD, StallE, StallM;
+    	wire FlushD, FlushE, FlushM;
+
+	wire [1:0] ForwardAE, ForwardBE;
+
+	wire [31:0] InstrD, SrcAE, SrcBE;
+    	wire [31:0] ALUResultE, ExtImmE, WriteDataE;
+    	wire [3:0] WA3E;
+
+	wire BranchPredicted;
+    	wire BranchTaken;
+    	wire FlushBranch;
+	
 	controller c(
 		.clk(clk),
 		.reset(reset),
-		.Instr(Instr[31:12]),
+		.Instr(InstrD[31:12]),
 		.ALUFlags(ALUFlags),
 		.RegSrc(RegSrc),
 		.RegWrite(RegWrite),
@@ -40,6 +46,21 @@ module arm (
 		.MemtoReg(MemtoReg),
 		.PCSrc(PCSrc)
 	);
+
+	hazard_unit h (
+        	.InstrD(InstrD),
+		.InstrE({InstrD[15:0]}), // todavia podria cambiarlo
+        	.RegWriteE(RegWrite),
+        	.RegWriteM(MemWrite),
+        	.ForwardAE(ForwardAE),
+        	.ForwardBE(ForwardBE),
+        	.StallF(StallF),
+        	.StallD(StallD),
+        	.StallE(StallE),
+        	.FlushD(FlushD),
+        	.FlushE(FlushE)
+    	);
+	
 	datapath dp(
 		.clk(clk),
 		.reset(reset),
@@ -52,9 +73,26 @@ module arm (
 		.PCSrc(PCSrc),
 		.ALUFlags(ALUFlags),
 		.PC(PC),
-		.Instr(Instr),
-		.ALUResult(ALUResult),
-		.WriteData(WriteData),
-		.ReadData(ReadData)
+		.InstrF(InstrF),
+		.ALUResultM(ALUResultM),
+        	.WriteDataM(WriteDataM),
+        	.ReadData(ReadData),
+        	.StallF(StallF),
+        	.FlushD(FlushD),
+        	.StallD(StallD),
+        	.FlushE(FlushE),
+        	.StallE(StallE),
+        	.FlushM(FlushM)
 	);
+
+	branch_predictor bp (
+        	.clk(clk),
+        	.reset(reset),
+        	.InstrF(InstrF),
+        	.BranchPredicted(BranchPredicted),
+        	.BranchTaken(BranchTaken),
+        	.FlushBranch(FlushBranch)
+    	);
+
+	assign FlushD = FlushBranch;
 endmodule
