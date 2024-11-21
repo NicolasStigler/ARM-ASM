11795ffdd3b5b135c00e39fab36bd786058c4ed8
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-21 16:53:29 -0500

    Update pipelineit.v

--- project_5/project_5.srcs/sources_1/new/pipelineit.v
+++ project_5/project_5.srcs/sources_1/new/pipelineit.v
@@ -1,14 +1,16 @@
 `timescale 1ns / 1ps
 
 module pipelineit(
-    input wire  i,
-    output wire o,
-    input wire clk
+    input wire [31:0] i,
+    output wire [31:0] o,
+    input wire clk,
+    input wire reset,
+    input wire stall
 );
-    reg next;
-    always @ (posedge clk) 
-        next <= i;
-        
-    assign o = next;
-
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            o <= 0;
+        else if (!stall)
+            o <= i;
+    end
 endmodule
