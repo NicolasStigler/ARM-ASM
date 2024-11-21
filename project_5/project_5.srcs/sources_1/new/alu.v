699dc470c9c30d5c8863f184a0958885da6396ec
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-21 13:05:12 -0500

    Update alu.v
    ALU soporta:
      ops logicas
      shift y rot
      saturacion
    
    ALUControl y flags arregladas
    simplificacion del codigo

--- project_5/project_5.srcs/sources_1/new/alu.v
+++ project_5/project_5.srcs/sources_1/new/alu.v
@@ -1,37 +1,44 @@
 `timescale 1ns / 1ps
 
 module alu (
-    input [31:0] a,
-    b,
-    input [1:0] ALUControl,
+    input signed [31:0] a, b,
+    input [3:0] ALUControl,
+    input Saturated,
     output reg [31:0] Result,
-    output wire [3:0] ALUFlags
+    output wire [3:0] ALUFlags // Negative, Zero, Carry, oVerflow
 );
-  wire neg, zero, carry, overflow;
-  wire [31:0] sum, condinvb, res_sub, res_reverse_sub;
-  wire [32:0] sum_extended;
+    wire [31:0] condinvb;
+    wire [32:0] sum_extended;
+    wire [31:0] logic_result;
+    wire signed [31:0] saturated_sum;
+    wire signed [31:0] saturated_sub;
 
-  assign condinvb = ALUControl[0] ? ~b : b;
-  assign sum_extended = a + condinvb + ALUControl[0];
-  assign res_sub = a + (~b + 1);
-  assign res_reverse_sub = a + (~b + 1);
+    assign condinvb = ALUControl[0] ? ~b : b;
+    assign sum_extended = {1'b0, a} + {1'b0, condinvb} + ALUControl[0]; // {1'b0, num}: num -> 0num
+    assign saturated_sum = (sum_extended[31] == 1'b0 && sum_extended[30:0] > $signed(32'b01111111111111111111111111111111)) ? 32'b01111111111111111111111111111111 : (sum_extended[31] == 1'b1 && sum_extended[30:0] < $signed(32'h10000000000000000000000000000000)) ? 32'h10000000000000000000000000000000 : sum_extended[31:0];
+    assign saturated_sub = (a > 0 && b < 0 && a - b < 0) ? 32'b01111111111111111111111111111111 : (a < 0 && b > 0 && a - b > 0) ? 32'h10000000000000000000000000000000 : a - b;
+    assign logic_result = (ALUControl[1:0] == 2'b00) ? a & b : (ALUControl[1:0] == 2'b01) ? a | b : (ALUControl[1:0] == 2'b10) ? a ^ b : ~(a ^ b);
+    
+    always @(*) begin
+        case (ALUControl)
+            4'b0000: Result = sum_extended[31:0]; // ADD
+            4'b0001: Result = sum_extended[31:0]; // SUB
+            4'b0010: Result = a & b; // AND
+            4'b0011: Result = a | b; // OR
+            4'b0100: Result = a ^ b; // XOR
+            4'b0101: Result = ~(a ^ b); // XNOR
+            4'b0110: Result = a << b[4:0]; // LSL
+            4'b0111: Result = a >> b[4:0]; // LSR
+            4'b1000: Result = a >>> b[4:0]; // ASR
+            4'b1001: Result = {a[0], a[31:1]}; // ROR
+            4'b1010: Result = Saturated ? saturated_sum : sum_extended[31:0]; // QADD
+            4'b1011: Result = Saturated ? saturated_sub : a - b; // QSUB
+            default: Result = 32'b0; // NOP
+        endcase
+    end
 
-  always @(*) begin
-    casex (ALUControl)
-      3'b000: Result = sum_extended[31:0];
-      3'b001: Result = res_sub;
-      3'b010: Result = a & b;
-      3'b011: Result = a | b;
-      3'b100: Result = a ^ b;
-      3'b101: Result = ~(a ^ b);
-      3'b110: Result = res_reverse_sub;
-      default: Result = 32'b0;
-    endcase
-  end
-
-  assign neg = Result[31];
-  assign zero = (Result == 32'b0);
-  assign carry = (ALUControl[0] == 1'b0) & sum_extended[32];
-  assign overflow = (ALUControl[0] == 1'b0) & ((a[31] & b[31] & ~Result[31]) | (~a[31] & ~b[31] & Result[31]));
-  assign ALUFlags = {neg, zero, carry, overflow};
+    assign ALUFlags[3] = Result[31];
+    assign ALUFlags[2] = (Result == 32'b0);
+    assign ALUFlags[1] = sum_extended[32];
+    assign ALUFlags[0] = (a[31] == b[31]) && (a[31] != Result[31]);
 endmodule
