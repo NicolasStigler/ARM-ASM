fb5b7cf83c6af785ec2d8192453000e616aa4f3d
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-26 18:23:16 -0500

    Update flopr.v

--- project_5/project_5.srcs/sources_1/new/flopr.v
+++ project_5/project_5.srcs/sources_1/new/flopr.v
@@ -1,19 +1,15 @@
 `timescale 1ns / 1ps
 
-module flopr (
-	clk,
-	reset,
-	d,
-	q
+module flopr #(parameter WIDTH = 8) (
+	input clk,
+	input reset,
+	input [WIDTH-1:0] d,
+	output [WIDTH-1:0] q
 );
-	parameter WIDTH = 8;
-	input wire clk;
-	input wire reset;
-	input wire [WIDTH - 1:0] d;
-	output reg [WIDTH - 1:0] q;
-	always @(posedge clk or posedge reset)
+	always @(posedge clk or posedge reset) begin
 		if (reset)
 			q <= 0;
 		else
 			q <= d;
+	end
 endmodule
