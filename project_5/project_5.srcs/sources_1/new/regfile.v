0b9b507dc35fb87586ea54c067911f254300ef9c
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-25 22:31:00 -0500

    regfile.v comentado

--- project_5/project_5.srcs/sources_1/new/regfile.v
+++ project_5/project_5.srcs/sources_1/new/regfile.v
@@ -1,29 +1,21 @@
 `timescale 1ns / 1ps
 
 module regfile (
-	clk,
-	we3,
-	ra1,
-	ra2,
-	wa3,
-	wd3,
-	r15,
-	rd1,
-	rd2
+	input clk,
+	input we3,
+	input [3:0] ra1, ra2, wa3,
+	input [31:0] wd3, r15,
+	output [31:0] rd1, rd2
 );
-	input wire clk;
-	input wire we3;
-	input wire [3:0] ra1;
-	input wire [3:0] ra2;
-	input wire [3:0] wa3;
-	input wire [31:0] wd3;
-	input wire [31:0] r15;
-	output wire [31:0] rd1;
-	output wire [31:0] rd2;
-	reg [31:0] rf [14:0];
-	always @(posedge clk)
+	reg [31:0] rf [0:14]; // register file de 15 elementos
+
+	// Op de escritura
+	always @(negedge clk) begin
 		if (we3)
 			rf[wa3] <= wd3;
+	end
+
+	// Ops de lectura
 	assign rd1 = (ra1 == 4'b1111 ? r15 : rf[ra1]);
 	assign rd2 = (ra2 == 4'b1111 ? r15 : rf[ra2]);
 endmodule
